43f44d8c64a4c0f607c7c2278d941f685a37ab90
bdeaton <github@briandeaton.net>
2014-01-02 15:35:54 -0800

    twitter link added

--- js/footer.js
+++ js/footer.js
@@ -15,6 +15,10 @@ onlineForLife.Footer = {
 		$('.feed-share-fb').on('click',function(){
 			window.location.href = "https://www.facebook.com/sharer/sharer.php?u=www.onlineforlife.org/2-1app/share.html";
 		});
+		$('.feed-share-twitter').on('click',function(){
+			window.location.href = "https://twitter.com/intent/tweet?text=Sign%20up%20to%20connect%20with%20me%20on%20Online%20For%20Life&url=http%3A%2F%2Fwww.onlineforlife.org/2-1app/share.html?id=2123";
+		});
+		
 		
 	}
 	
