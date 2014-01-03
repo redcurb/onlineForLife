be4e2cab31681ef9014725b9de74715d372e6721
bdeaton <github@briandeaton.net>
2014-01-02 16:28:07 -0800

    redirect added

--- js/footer.js
+++ js/footer.js
@@ -18,9 +18,11 @@ onlineForLife.Footer = {
 			var imageUrl = encodeURIComponent('http://onlineforlife.org/2-1app/olf-logo.png');
 			var userId = 2313;
 			var shareUrl = encodeURIComponent('http://onlineforlife.org/2-1app/share.html?shareId='+userId);
-			var fbLink = 'http://www.facebook.com/sharer.php?s=100&p[title]='+ title + '&p[summary]=' + description + '&p[url]=' + shareUrl + '&p[images][0]=' + imageUrl;
-			//window.location.href = "https://www.facebook.com/sharer/sharer.php?u=www.onlineforlife.org/2-1app/share.html?id=2123";
-			window.location.href = fbLink;
+			//https://m.facebook.com/sharer.php?u=http://onlineforlife.org/2-1app/share.html?shareId=2313&_rdr&p=shkjshjks
+			var fbLink = 'http://m.facebook.com/sharer.php?p[title]='+ title + '&p[summary]=' + description + '&p[url]=' + shareUrl + '&p[images][0]=' + imageUrl;
+			
+			window.location.href = "https://www.facebook.com/sharer/sharer.php?m2w&u=www.onlineforlife.org/2-1app/share.html?id=2123";
+			//window.location.href = fbLink;
 		});
 		$('.feed-share-twitter').on('click',function(){
 			window.location.href = "https://twitter.com/intent/tweet?text=Sign%20up%20to%20connect%20with%20me%20on%20Online%20For%20Life&url=http%3A%2F%2Fwww.onlineforlife.org/2-1app/share.html?id=2123";
