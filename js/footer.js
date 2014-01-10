c56527220e3b4f7e98422edf14edc2702690cdc8
bdeaton <github@briandeaton.net>
2014-01-10 15:49:49 -0800

    share links removed

--- js/footer.js
+++ js/footer.js
@@ -10,9 +10,12 @@ onlineForLife.Footer = {
 	setupHandlers: function(){
 		//console.log('footer setupHandlers');
 		$('.feed-share-sms').on('click',function(){
-			window.location.href = "sms:6268647084?body=TESTMESSAGE";
+			return false;
+			//window.location.href = "sms:6268647084?body=TESTMESSAGE";
 		});
 		$('.feed-share-fb').on('click',function(){
+			return false;
+			/*
 			var title = encodeURIComponent('Online For Life');
 			var description = encodeURIComponent('So Every Child Makes Their Mark. Our Mission: Providing comprehensive care to abortion-determined women and men in their deepest time of need.');
 			var imageUrl = encodeURIComponent('http://onlineforlife.org/2-1app/olf-logo.png');
@@ -23,9 +26,11 @@ onlineForLife.Footer = {
 			
 			window.location.href = "https://www.facebook.com/sharer/sharer.php?m2w&u=www.onlineforlife.org/2-1app/share.html?id=2123";
 			//window.location.href = fbLink;
+			*/
 		});
 		$('.feed-share-twitter').on('click',function(){
-			window.location.href = "https://twitter.com/intent/tweet?text=Sign%20up%20to%20connect%20with%20me%20on%20Online%20For%20Life&url=http%3A%2F%2Fwww.onlineforlife.org/2-1app/share.html?id=2123";
+			return false;
+			//window.location.href = "https://twitter.com/intent/tweet?text=Sign%20up%20to%20connect%20with%20me%20on%20Online%20For%20Life&url=http%3A%2F%2Fwww.onlineforlife.org/2-1app/share.html?id=2123";
 		});
 
 
