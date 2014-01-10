bfe5fe4f1234a15188be3500c3d3e8196ebd528e
bdeaton <github@briandeaton.net>
2014-01-10 12:43:19 -0800

    settings toggle

--- js/settings.js
+++ js/settings.js
@@ -7,6 +7,26 @@ onlineForLife.Settings = {
 	},
 	
 	setupHandlers: function(){
+		
+		$('.settings-list li').on('click',function(){
+			$this = $(this);
+			var $content = $this.find('.settings-content');
+			var isOpen = $content.hasClass('content-open');
+			console.log('isOpen:' + isOpen);
+			if(isOpen){
+				$content.hide('blind',{},500,function(){
+					console.log('done');
+					$content.toggleClass('content-open');
+				});
+			}
+			else{
+				$content.show('blind',{},500,function(){
+					console.log('done');
+					$content.toggleClass('content-open');
+				});
+			}
+			//$('.mypanel-left h2.logo').animate({'top':'0'}, 300);
+		});
 		$('.link-settings-ofl').on('click',function(){
 			var url = 'http://www.onlineforlife.org';
 			//window.location.href = 'events.html';
@@ -24,42 +44,6 @@ onlineForLife.Settings = {
 			myURL = encodeURI(url);
 			window.open(url, '_blank', 'location=yes'); 
 			
-		});
-		
-		$('.link-settings-ofl-2').on('click',function(){
-			$('h2 p').text('test text');
-		});
-		$('#blank-loc-tool-back').on('click',function(){
-			//does NOT work - confirmed
-			var id = $(this).attr('id');
-			$('h2 p').text(id);
-			var url = 'http://www.onlineforlife.org';
-			myURL = encodeURI(url);
-			window.open(url, '_blank', 'location=yes,toolbar=yes', 'closebuttoncaption=back'); 
-		});
-		$('#self-yes-back').on('click',function(){
-			//does NOT work - confirmed
-			var id = $(this).attr('id');
-			$('h2 p').text(id);
-			var url = 'http://www.onlineforlife.org';
-			myURL = encodeURI(url);
-			window.open(url, '_self', 'location=yes', 'closebuttoncaption=back'); 
-		});
-		$('#self-yes').on('click',function(){
-			//does NOT work - confirmed
-			var id = $(this).attr('id');
-			$('h2 p').text(id);
-			var url = 'http://www.onlineforlife.org';
-			myURL = encodeURI(url);
-			window.open(url, '_self', 'location=yes'); 
-		});
-		$('#loadUrl').on('click',function(){
-			//does NOT work
-			var id = $(this).attr('id');
-			$('h2 p').text(id);
-			var url = 'http://www.onlineforlife.org';
-			myURL = encodeURI(url);
-			navigator.app.loadUrl(url, { openExternal:true });
 		});
 		
 		
