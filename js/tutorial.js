8c3453a802e92acd016af77cc8880f726febb877
bdeaton <github@briandeaton.net>
2014-01-13 00:47:21 -0800

    scroll close tut

--- js/tutorial.js
+++ js/tutorial.js
@@ -11,20 +11,23 @@ onlineForLife.Tutorial = {
 		var dbUrl = 'https://ofl.firebaseio.com/tutorial/text/';
 		var myDataRef = new Firebase(dbUrl);
 		myDataRef.on('child_added', function(snapshot) {
+			var tutorialData = snapshot.val();
+			console.log(tutorialData);
 			var tutorialId = snapshot.name();
-			var tutorialText = snapshot.val();
-			console.log(tutorialId + ' - ' + tutorialText);
-			var itemHtml = onlineForLife.Tutorial.buildTutorialItem(tutorialId, tutorialText);
+			var tutorialText = tutorialData.text;
+			var tutorialTextAlignment = tutorialData.align;
+			console.log(tutorialId + ' - ' + tutorialText + ' - ' + tutorialTextAlignment);
+			var itemHtml = onlineForLife.Tutorial.buildTutorialItem(tutorialId, tutorialText, tutorialTextAlignment);
 			console.log(itemHtml);
 			$tutorial.append(itemHtml);
 		});
 		
 	},
 	
-	buildTutorialItem: function(id, text){
+	buildTutorialItem: function(id, text, align){
 		var source   = $("#template-tutorial-item").html();
 		var template = Handlebars.compile(source);
-		var context = {id:id, text:text}
+		var context = {id:id, text:text, align: align}
 		var html = template(context);
 		return html;
 	},
@@ -37,11 +40,22 @@ onlineForLife.Tutorial = {
 				console.log('cta');
 				onlineForLife.Tutorial.handleItemToggle($(this));
 			});
+			$(window).on('scroll',function() {
+				$('a.tutorial-hide').click();
+			});
 		});
+		/*
+		$('#content-tutorial').not('.tutorial-item').on('click', function(event){
+			event.stopPropagation();
+		});
+		*/
+		
+		
 		$('a.tutorial-hide').on('click', function(){
 			$('#modal-screen').removeClass('screen-tutorial').hide();
 			$('#content-tutorial').addClass('tutorial-closed').removeClass('tutorial-open');
 			$('.tutorial-item .tutorial-cta').off('click');
+			$(window).off('scroll');
 		});
 	},
 	
