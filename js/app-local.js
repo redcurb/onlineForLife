ad19cacd41206a52e27561d71ca9d6772628b871
bdeaton <github@briandeaton.net>
2014-01-17 18:46:43 -0800

    submit v1.05

--- js/app-local.js
+++ js/app-local.js
@@ -1,4 +1,17 @@
 $(function() {
-	onlineForLife.Auth.init();
+	function checkConnection(){
+		if(window.navigator.onLine){
+			console.log('has connection');
+			onlineForLife.Auth.init();
+		}
+		else {
+			console.log('has NO connection');
+			alert("You're currently not connected to the internet.");
+			setTimeout(function(){
+				location.reload();
+			}, 3000);
+		}
+	}
+	checkConnection();
 });
 
